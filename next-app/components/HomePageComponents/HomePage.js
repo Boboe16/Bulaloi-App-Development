87a7dbc8b1941a487ab24a45489bf50f34a6b6dd
Boboe16 <jerixmodz@gmail.com>
2023-10-26 00:19:22 +0800

    yamete

--- next-app/components/HomePageComponents/HomePage.js
+++ next-app/components/HomePageComponents/HomePage.js
@@ -1,9 +1,13 @@
+import { Head } from 'next/head'
 import Header from '../HeaderComponents/Header.js'
 import Content from './Content.js'
 
 function HomePage({ data }) {
 	return (
 		<>
+			<Head>
+				<meta name="google-site-verification" content="rvTVa5KFKGG9g5QcehEZVOLpdbqlLlhJ_tRQRlYykhw" />
+			</Head>
 			<Header />
 			<Content data={ data } />
 		</>
